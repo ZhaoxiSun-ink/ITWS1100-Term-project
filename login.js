4d8b425e671f700917c140cc4f83fd9ffa46d49a
ZhaoxiSun19991225 <47791231+ZhaoxiSun19991225@users.noreply.github.com>
2019-03-31 12:31:43 -0400

    Create login.js
    This code cannot work on local because of a protected policy. Professor says it will work when we put it on the web.

---
+++ login.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $("#save").click(function ()  {
+        var name = $("#name").val();
+        var password = $("#password").val();
+        //response.setHeader("Access-Control-Allow-Origin", "*"); 
+        $.ajax({
+            type:'GET',
+            url: 'usersdata.json',
+            dataType:"json",
+            error:function(){alert("fail")},
+            success:function(data){
+                console.log(data);
+                var len = data.user.length;
+                for (var i = 0; i <len; i++){
+                    if(name == data.user[i].name && password == data.user[i].password){
+                        alert("login succcessful.");
+                        return;
+                    }
+                }
+                alert("login fail");
+            }
+        })
+    });
+});
